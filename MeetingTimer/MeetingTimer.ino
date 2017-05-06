90c5e1a4b785c989d8782ce338a7f6f6b8f8ebd7
Michael <Michael@STRONGBAD>
2017-05-06 06:54:19 -0400

    Add project files.

---
+++ MeetingTimer/MeetingTimer.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MeetingTimer.ino
+ Created:	5/6/2017 6:54:03 AM
+ Author:	Michael
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
